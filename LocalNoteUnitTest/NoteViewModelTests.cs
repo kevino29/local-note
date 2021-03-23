9c976ac661445ef674159e1a30eb103916ad38e3
Kevin Orenday <varax297@gmail.com>
2021-03-23 20:12:10 -0300

    added a ui text method, still fails

---
+++ LocalNoteUnitTest/NoteViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocalNote.ViewModels;
+
+namespace LocalNoteUnitTest
+{
+    [TestClass]
+    public class NoteViewModelTests
+    {
+        private NoteViewModel vm = new NoteViewModel();
+
+        [TestMethod]
+        public void Test_ReadyOnly_WhenEditModeIsTrue()
+        {
+            vm.EditMode = true;
+            bool expected = false;
+
+            Assert.AreEqual(expected, vm.ReadOnly);
+        }
+    }
+}
---
+++ LocalNoteUnitTests/UnitTest.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocalNote.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+
+namespace LocalNoteUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [UITestMethod]
+        public void Test_ReadOnly_WhenEditMode()
+        {
+            NoteViewModel vm = new NoteViewModel();
+            vm.EditMode = true;
+
+            // THIS IS WRONG! IT SHOULD BE FALSE!!!
+            Assert.IsTrue(vm.ReadOnly);
+        }
+    }
+}
